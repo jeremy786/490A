726efded6d357d64a25603f776a53a7e4256bf42
jeremy786 <jeremiahstpierre@yahoo.com>
2021-02-20 20:59:14 -0800

    UPdate server

--- api.py
+++ api.py
@@ -1,6 +1,13 @@
 import flask
 import sys
+import time
+import RPi.GPIO as GPIO
 from flask import request, jsonify,send_from_directory, redirect, url_for
+from flask import make_response
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+GPIO.output(24, 1)
 
 test = [{
     "key":"val",
@@ -17,7 +24,7 @@ test = [{
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] =  True
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 @app.route("/",methods=["GET"])
 def home():
     return "<h1>Welcome</h1>"
@@ -33,15 +40,21 @@ def form_test():
     if request.method == "GET":
         return app.send_static_file('form.html')
     else:
-        data= request.form["test"]
-        print(data, flush=True)
-    return jsonify(success=True)
+        data= request.form
+        print(data["text"], flush=True)
+        GPIO.output(24,0)
+        time.sleep(float(request.form["time"]))
+        GPIO.output(24,1)
+    return make_response(jsonify(data),200)
 
 @app.route("/kinect",methods=["POST"])
 def kinect():
     result = request.get_json()
     print(result, flush=True)
-    return jsonify(success=True)
+    GPIO.output(24,0)
+    time.sleep(0.5)
+    GPIO.output(24,1)
+    return make_response(jsonify(result),200)
 
 @app.errorhandler(404)
 def page_not_found(e):
